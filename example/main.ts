80b6ddec2d3b4501c209f11e742d01e1a7aeb328
Marotik <kitoram3@gmail.com>
2025-03-13 23:30:51 +0900

    [FEAT]add using global pipes

--- example/main.ts
+++ example/main.ts
@@ -1,15 +1,21 @@
 // 애플리케이션 시작점 
-
 import "reflect-metadata";
 import { NestFactory } from "../src/core/factory";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "../src/core/pipes/validation.pipe";
 
 
 async function bootstrap() {
+    /* 애플리케이션 생성 */
     const app = await NestFactory.create(AppModule, {
         abortOnError: false,
         autoFlushLogs: true,
     });
+
+    /* Global Pipe 설정 */
+    NestFactory.useGlobalPipes(new ValidationPipe({ optional: true }));
+
+    /* 서버 실행 */
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     })
